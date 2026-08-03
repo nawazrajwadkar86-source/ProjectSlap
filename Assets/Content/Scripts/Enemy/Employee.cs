06c2480ec81c906f9700008f7e7f528a710d97aa
uprealmrx <uprealmrx@gmail.com>
2026-08-03 12:32:57 +0530

    coins added

--- Assets/Content/Scripts/Enemy/Employee.cs
+++ Assets/Content/Scripts/Enemy/Employee.cs
@@ -20,11 +20,11 @@ public class Employee : Target
     }
     protected override void UpdateMultiSlapMeter(ETargetType type)
     {
-        MultiSlap.multiSlap_instance.slider.fillAmount += MultipleSlapValue;
+      //  MultiSlap.multiSlap_instance.slider.fillAmount += MultipleSlapValue;
     }
     protected override void UpdateHeatMeter(ETargetType type)
     {
-        HeatMeter.HeatMeter_Instance.Heat_val += HeatIncreaseValue;
+      //  HeatMeter.HeatMeter_Instance.Heat_val += HeatIncreaseValue;
     
     }
 }
